e31d253e9078da2e3d975fb9f93c1bd91733fe64
Bhanu Chaitanya <143734650+chaitanyabhanu@users.noreply.github.com>
2024-08-21 23:41:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// //  JavaScript -Objects 
+// const person1 = {
+
+// list :"1.Example of objects",
+// firstName: " Bhanu " ,
+// lastName:"Chaitanya",
+// age: 24,
+// isheEmployed:true,
+
+// sayHello : function() { console.log("Hi ! I am bhanu chaitanya!")},
+//     sayBye: function(){ console.log("Goodbye")}
+// };
+// document.getElementById("js-objects").innerHTML=person1.firstName;
+// // 
+// const person2={
+//     list:"2.Example",
+//     firstName:"Pratap",
+//     lastName:"Raja",
+//     age:22,
+//     isheEmployed:te=true,
+//     sayHello:function(){  console.log("Hello ! i am Pratap Raja!") }
+
+// };
+// person1.sayHello();
+// person2.sayHello();
+
+
+
+//  JavaScript -Arrays
+
+let    Honda = [ "Unicorn","Shine","Cd110", "CBshine" ];
+  
+Honda.sort();   
+console.log(Honda);
+
+
